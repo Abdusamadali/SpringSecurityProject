2666040fc50f2940805fcbfe20dc583222d6b85a
Abdusamadali <abdussamadali05@gmail.com>
2025-10-03 11:40:04 +0530

    added roleBased Authorization and requestMathers

--- src/main/java/com/abdus/springsecurityproject/controller/HealthCheck.java
+++ src/main/java/com/abdus/springsecurityproject/controller/HealthCheck.java
@@ -1,22 +1,29 @@
 package com.abdus.springsecurityproject.controller;
 
-import jdk.jshell.Snippet;
-import org.apache.coyote.Response;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+
+import jakarta.annotation.PreDestroy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.transform.OutputKeys;
 
 
 @RestController
 public class HealthCheck {
-    @GetMapping("/health-check")
+    @GetMapping("/public")
     public  ResponseEntity healthCheck() {
         return  new  ResponseEntity<>("hello World", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public  ResponseEntity user() {
+        return  new  ResponseEntity<>("hello User", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public  ResponseEntity admin() {
+        return  new  ResponseEntity<>("hello admin", HttpStatus.OK);
     }
 }
--- src/main/java/com/abdus/springsecurityproject/filters/SecurityConfig.java
+++ src/main/java/com/abdus/springsecurityproject/filters/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,12 +20,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityConfigFunction(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(req->req.anyRequest().authenticated())
-                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //making session stateless --> no cookies
+        http.authorizeHttpRequests(req->req
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+//                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //making session stateless --> no cookies
+
         .httpBasic(withDefaults());
         return http.build();
     }
