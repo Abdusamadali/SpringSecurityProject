d22f50f278d2d17d0ad1e1901e522e8c5afe508b
Abdusamadali <abdussamadali05@gmail.com>
2025-10-03 12:29:12 +0530

    added h2-console

--- src/main/java/com/abdus/springsecurityproject/filters/SecurityConfig.java
+++ src/main/java/com/abdus/springsecurityproject/filters/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,11 +29,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityConfigFunction(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(req->req
                         .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
 //                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //making session stateless --> no cookies
-
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))//disabling clickJacking protection for render in side iframe
         .httpBasic(withDefaults());
+
         return http.build();
     }
 }
